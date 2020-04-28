b81f06bb0f365f8b1679619b1a7b74fe9d373445
eunjung04 <peunj04@gmail.com>
2020-04-28 19:45:11 +0900

    로그인화면 XML

--- app/src/main/java/com/example/api02/SplashActivity.kt
+++ app/src/main/java/com/example/api02/SplashActivity.kt
@@ -5,11 +5,11 @@ import android.os.Bundle
 
 class SplashActivity : BaseActivity() {
     override fun setupEvents() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun setValue() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
