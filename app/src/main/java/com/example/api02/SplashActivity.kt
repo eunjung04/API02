894c47b92b99321f0af862c4fc1ffc8bad416106
eunjung04 <peunj04@gmail.com>
2020-04-28 19:51:27 +0900

    스플래쉬액티비티 기초 구현

--- app/src/main/java/com/example/api02/SplashActivity.kt
+++ app/src/main/java/com/example/api02/SplashActivity.kt
@@ -1,7 +1,9 @@
 package com.example.api02
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashActivity : BaseActivity() {
     override fun setupEvents() {
@@ -9,6 +11,15 @@ class SplashActivity : BaseActivity() {
     }
 
     override fun setValue() {
+
+        //2.5초동안 보여준 뒤=>로그인 액티비티로 전환
+        Handler().postDelayed({
+
+            val myIntent=Intent(mContext, LoginActivity::class.java)
+            startActivity(myIntent)
+            finish()
+
+        }, 2500)
 
     }
 
