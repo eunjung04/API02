982d2042b5b8280a1e17aabf7365927633e2c414
eunjung04 <peunj04@gmail.com>
2020-04-29 20:53:00 +0900

    게시글 파싱 기능 작성중 =>User 클래스의 파싱 기능 재활용

--- app/src/main/java/com/example/api02/datas/Post.kt
+++ app/src/main/java/com/example/api02/datas/Post.kt
@@ -1,8 +1,27 @@
 package com.example.api02.datas
 
+import org.json.JSONObject
 import java.util.*
 
 class Post {
+
+    companion object{
+        fun getPostFromJson(json : JSONObject) : Post {
+
+            val p=Post()
+
+            p.phoneNum=json.getString("phone_num")
+            p.title=json.getString("title")
+            p.content=json.getString("content")
+
+            val userJson=json.getJSONObject("writer")
+            p.writer=User.getUserFromJsonObject(userJson)
+
+
+            return p
+
+        }
+    }
 
     var phoneNum=""
     var title=""
