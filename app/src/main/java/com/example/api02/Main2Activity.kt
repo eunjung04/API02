814a51a27be5adff4cabf62b000244ad2c33fe62
eunjung04 <peunj04@gmail.com>
2020-04-29 19:35:37 +0900

    메인 액티비티 추가 -서버 url 변경

---
+++ app/src/main/java/com/example/api02/Main2Activity.kt
@@ -0,0 +1,22 @@
+package com.example.api02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main2Activity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setValue()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValue() {
+
+    }
+}
